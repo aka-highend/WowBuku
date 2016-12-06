0662a8ed5ae84475764dbf592de4c755fac664fc
Mohamad Fachrur Ridwan <ridwan.fachrur@gmail.com>
2016-12-06 13:40:04 +0700

    Set realm configuration

--- app/src/main/java/com/rfachrur/wowbuku/activity/MainActivity.java
+++ app/src/main/java/com/rfachrur/wowbuku/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.rfachrur.wowbuku;
+package com.rfachrur.wowbuku.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.rfachrur.wowbuku.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/rfachrur/wowbuku/app/WowBukuApp.java
@@ -0,0 +1,26 @@
+package com.rfachrur.wowbuku.app;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by rfachrur on 12/6/16.
+ *
+ */
+
+public class WowBukuApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this)
+                .name(Realm.DEFAULT_REALM_NAME)
+                .schemaVersion(0)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+    
+}
